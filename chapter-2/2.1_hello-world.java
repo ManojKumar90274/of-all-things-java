d7372949e231d73bb48ee96bc97b38f4facb9ed3
clerizedd <casucolerizedoreen+github@gmail.com>
2022-10-15 23:34:10 +0800

    Update 2.1_hello-world.java

--- chapter-2/2.1_hello-world.java
+++ chapter-2/2.1_hello-world.java
@@ -1,14 +1,10 @@
-/**         A program that displays "Hello World!"        **/
+/** A program to display the message "Hello World!" on standard output */
 
-public class HelloWorld{
-  public static void main (string[]args){
-    System.out.println ("Hello World");
+class HelloWorld {
+  public static void main (String[]args){
+    System.out.println("Hello World!");
   }
-}
+} // end of class Hello World
 
-/**
-end of class HelloWorld
-the command that displays the message is the system.out.println ("hello world!")
-This command is an example of a subroutine call statement. It uses a “built-in subroutine”
-named System.out.println to do the actual work.
-**/
+/** This command is an example of a subroutine call statement. It uses a “built-in subroutine” named System.out.println to do the actual work.
+ */
