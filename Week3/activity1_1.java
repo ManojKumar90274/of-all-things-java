701045573f738fecaa4cec6bb051299a8a088b1e
clerizedd <casucolerizedoreen+github@gmail.com>
2022-12-23 21:41:12 +0800

    -

--- Week3/activity1_1.java
+++ Week3/activity1_1.java
@@ -1,5 +1,4 @@
 /*  Create a program that will check whether a number is positive or negative */
-
 package Week3;
 import java.util.Scanner;
 
@@ -7,6 +6,22 @@ public class activity1_1 {
     public static void main (String [] args){
         Scanner sc = new Scanner (System.in);
         int x, y=0;
+
+        while (y<3){
+            System.out.print ("Enter a number:  ");
+            x = sc.nextInt();
+
+            if (x>0){
+                System.out.println ("Positive number");
+            }
+            else if (x<0){
+                System.out.println ("Negative number");
+            }
+            else {
+                System.out.println ("The number is zero");
+            }
+
+            y++;
+        }
     }
-    
 }
