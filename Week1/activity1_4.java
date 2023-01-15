4782b7be25615c837347dcbc065ca97c2a00c5c7
clerizedd <casucolerizedoreen+github@gmail.com>
2023-01-15 20:15:23 +0800

    -

---
+++ Week1/activity1_4.java
@@ -0,0 +1,15 @@
+/* Write a Java program that will ask for the name of your mother and print it.
+ * 
+ */
+
+
+package Week1;
+import java.util.Scanner;
+
+public class activity1_4 {
+    Scanner sc = new Scanner (System.in);
+
+    System.out.print ("What is your address?  ");
+    String x = sc.nextLine();
+    
+}
