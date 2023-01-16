af48de31695f71422d2a8769ae7ef0dde3a8a092
clerizedd <casucolerizedoreen+github@gmail.com>
2023-01-16 20:20:24 +0800

    Create activity1_6.java

---
+++ Week1/activity1_6.java
@@ -0,0 +1,17 @@
+/*  Write a Java program that will ask for your favorite
+    color and print it.
+ */
+
+ package Week1;
+ import java.util.Scanner;
+ 
+ public class activity1_6 {
+     public static void main (String [] args){
+         Scanner y = new Scanner (System.in);
+ 
+         System.out.print ("What is your favorite color?:   ");
+         String x = y.nextLine ();
+         System.out.println (x);
+     }
+ }
+ 
