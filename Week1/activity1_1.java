3cd7f90409f2612ed62ba55e7487cb606e3c88d5
clerizedd <casucolerizedoreen+github@gmail.com>
2023-01-16 20:30:58 +0800

    Create activity1_1.java

---
+++ Week1/activity1_1.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class activity1_1 {
+    public static void main (String [] args){
+        Scanner y = new Scanner (System.in);
+        System.out.print ("What is your favorite food?  ");
+        String x = y.nextLine ();
+        System.out.println (x);
+    }
+}
