20b0e4361ecd03a995ed12adb2d30ff36e66d523
clerizedd <casucolerizedoreen+github@gmail.com>
2023-01-16 20:33:35 +0800

    -

---
+++ Week1/activity1_4.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class activity1_4 {
+    public static void main (String [] args){
+        Scanner y = new Scanner (System.in);
+        System.out.print ("\n\nWhat is the name of your mother?  ");
+        String x = y.nextLine ();
+        System.out.println (x);
+    }
+}
